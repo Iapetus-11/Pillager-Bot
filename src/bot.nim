96faa7ba050325d8f63c013635fe17efdc64ead1
Milo Weinberg <iapetus011@gmail.com>
2021-09-06 14:21:42 -0400

    add basic bot

--- src/bot.nim
+++ src/bot.nim
@@ -1,9 +1,19 @@
-import dimscord
-import dimscmd
-import json
+import std/[json, asyncdispatch]
+import dimscord, dimscmd
 
 let
     config = parseJson(readFile("config.json"))
     client = newDiscordClient(config["token"].getStr())
 
-var 
+var cmd = newHandler(client)
+
+proc messageCreate(s: Shard, msg: Message) {.event(client).} =
+    let handledCommand = await cmd.handleMessage(config["prefix"].getStr(), s, msg)
+
+    if not handledCommand:
+        echo msg.content
+
+cmd.addChat("hello") do ():
+    discard await client.api.sendMessage(msg.channelID, "Hello!")
+
+waitFor client.startSession()
