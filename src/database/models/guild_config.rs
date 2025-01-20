784532b1f1a7bf6a5e4ba36533baa08bdfa0fbd9
Milo Weinberg <iapetus011@gmail.com>
2025-01-20 18:32:46 -0500

    Add linting & gh action

--- src/database/models/guild_config.rs
+++ src/database/models/guild_config.rs
@@ -4,7 +4,7 @@ use crate::database::schema::guild_configs;
 
 #[derive(Debug, Clone, Selectable, Queryable, Insertable, AsChangeset)]
 #[diesel(table_name = guild_configs, check_for_backend(diesel::pg::Pg))]
-#[changeset_options(treat_none_as_null = "true")]
+#[diesel(treat_none_as_null = true)]
 pub struct GuildConfig {
     pub id: i64,
     pub message_logging_channel_id: Option<i64>,
